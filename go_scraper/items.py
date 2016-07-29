27c84293ce3f359086dac7c3912b311f6ec17941
ChunPo Yeh <chunpo@indeed.com>
2016-07-29 16:28:34 -0500

    add items

--- go_scraper/items.py
+++ go_scraper/items.py
@@ -5,9 +5,13 @@ import scrapy
 class GoRecordItem(scrapy.Item):
     BoardSize = scrapy.Field()
     WhitePlayer = scrapy.Field()
+    WhiteRank = scrapy.Field()
     BlackPlayer = scrapy.Field()
+    BlackRank = scrapy.Field()
+    GameRecord = scrapy.Field()
     GameResult = scrapy.Field()
+    GameInfo = scrapy.Field()
+    Date = scrapy.Field()
     Location = scrapy.Field()
     Komi = scrapy.Field()
-    Date = scrapy.Field()
-    OtherInfo = scrapy.Field()
+
